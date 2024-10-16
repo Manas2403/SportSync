9b50266bc9d4727290d77d6e710c3d8e207a30e9
Manas2403 <guptamanas149@gmail.com>
2024-10-16 16:07:12 +0530

    add controllers and models

---
+++ backend/models/booking.model.js
@@ -0,0 +1,26 @@
+import { model, Schema } from "mongoose";
+const BookingSchema = new Schema(
+  {
+    user: {
+      type: String,
+      required: true,
+    },
+    court: {
+      type: Schema.Types.ObjectId,
+      ref: "Court",
+      required: true,
+    },
+    startTime: {
+      type: Date,
+      required: true,
+    },
+    endTime: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+export default model("Booking", BookingSchema);
