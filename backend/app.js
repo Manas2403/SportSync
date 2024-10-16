4c42064b81ddcdee1c277fd1c7e365a6df1b58b9
Manas2403 <guptamanas149@gmail.com>
2024-10-16 15:57:52 +0530

    Initial Setup

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+import connDB from './db.config.js';
+connDB();
+
+import centerRoutes from './routes/center.routes.js';
+app.use('/center', centerRoutes);
+
+import sportRoutes from './routes/sport.routes.js';
+app.use('/sport', sportRoutes);
+
+import courtRoutes from './routes/court.routes.js';
+app.use('/court', courtRoutes);
+
+import bookingRoutes from './routes/booking.routes.js';
+app.use('/booking', bookingRoutes);
+
+app.listen(
+    process.env.PORT ? process.env.PORT : 8080,
+    process.env.HOST ? process.env.HOST : '127.0.0.1',
+    console.log(
+      `listening on http://localhost:${
+        process.env.PORT ? process.env.PORT : 8080
+      }/`,
+    ),
+  );
